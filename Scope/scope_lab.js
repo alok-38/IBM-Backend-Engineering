ef1bb0061c8ec36f9d948637c85c5b0c9a9539a7
Alok <plantsfortrees@gmail.com>
2026-02-28 11:12:43 +0530

    Working with function scoped variables

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -9,3 +9,25 @@ const globalConst = "I'm a global constant";
     let blockLet = "I'm a block-scoped let";
     const blockConst = "I'm a block-scoped const";
 }
+
+// Global scope
+console.log(globalVar); // Output: "I'm a global variable"
+console.log(globalLet); // Output: "I'm also global, but scoped with let"
+console.log(globalConst); // Output: "I'm a global constant"
+
+// Local scope (testing)
+console.log(blockVar);
+console.log(blockLet);
+console.log(blockConst);
+
+function show() {
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a block-scoped const";
+}
+
+show();
+
+console.log(functionVar); // Throws ReferenceError
+console.log(functionLet); // Throws ReferenceError
+console.log(functionConst); // Throws ReferenceError
