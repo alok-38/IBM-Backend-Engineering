4a502a3aef6bce6336794b8e6322b7fdfdba07de
Alok <plantsfortrees@gmail.com>
2026-02-28 15:06:14 +0530

    Grocery Total Calculator

---
+++ calculateArea/calculate_total.js
@@ -0,0 +1,12 @@
+let grocery_1;
+let grocery_2;
+let grocery_3;
+
+function groceryTracker() {
+    grocery_1 = parseFloat(document.getElementById('grocery-1').value);
+    grocery_2 = parseFloat(document.getElementById('grocery-2').value);
+    grocery_3 = parseFloat(document.getElementById('grocery-3').value);
+
+    let total = grocery_1 + grocery_2 + grocery_3;
+    document.getElementById('total').innerText = `The total amount is: ${total}`;
+}
